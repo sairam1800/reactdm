4388e1fde6b14efd8b3d5ab7762185b26516f2ff
Ram <sairam1p618@gmail.com>
2025-05-10 16:05:09 +0530

    branch

--- src/App.jsx
+++ src/App.jsx
@@ -11,16 +11,6 @@ function App() {
       <textarea className="logo">
         <li>version textarea</li>
         <li>message</li>
-        <span className="logo">
-          "2025-05-10 15:36:55.179 [info] git blame --root --incremental
-          bbcc82af8df08376193cd0db08aa51cec5be0a7b -- .git/COMMIT_EDITMSG [65ms]
-          2025-05-10 15:36:55.179 [info] fatal: no such path .git/COMMIT_EDITMSG
-          in bbcc82af8df08376193cd0db08aa51cec5be0a7b 2025-05-10 15:38:04.240
-          [info] git ls-tree -l 99ff755d69f22ffbb661b5c90a920dc18faf60f2 --
-          src/redux/slice.js [76ms] 2025-05-10 15:38:04.241 [info] git ls-tree
-          -l 99ff755d69f22ffbb661b5c90a920dc18faf60f2 -- src/redux/store.js
-          [70ms]"
-        </span>
       </textarea>
     </div>
   );
